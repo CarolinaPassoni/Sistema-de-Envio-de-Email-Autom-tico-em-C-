0abf672441bac4be0a62c909222cb5725995dbd3
Carolina Passoni <105358062+CarolinaPassoni@users.noreply.github.com>
2025-11-04 14:52:50 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+class Atividade5
+{
+    static void Main()
+    {
+        Console.Write("Digite o nome da pasta que deseja excluir (em C:\\ExemploAula): ");
+        string nome = Console.ReadLine();
+        string caminho = $@"C:\ExemploAula\{nome}";
+
+        if (Directory.Exists(caminho))
+        {
+            Directory.Delete(caminho, true);
+            Console.WriteLine("Diretório excluído com sucesso!");
+        }
+        else
+        {
+            Console.WriteLine("Essa pasta não existe!");
+        }
+    }
+}
+
